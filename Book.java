a2a5f6e006c1d923cbb25965c918b774c4552f22
josephschildt <schildtjoey@gmail.com>
2024-11-07 11:46:06 -0600

    added simple functions to Book

--- Book.java
+++ Book.java
@@ -1,3 +1,26 @@
+
+
 public class Book {
-    
+    string bookTitle;
+    string author;
+    double price;
+
+
+    public Book(string bookTitle, string author, double price){
+      this.bookTitle = bookTitle;
+      this.author = author;
+      this.price = price;
+    }
+
+    public String getTitle(){
+      return bookTitle;
+    }
+
+    public String getAuthor(){
+      return author;
+    }
+
+    public double getPrice(){
+      return price;
+    }
 }
