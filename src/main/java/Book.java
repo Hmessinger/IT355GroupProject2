143d1d5256b29b081b7c089dd7898a4f36ffa34c
Daulton Widlacki <soldbuydaulton@gmail.com>
2024-11-07 12:25:03 -0600

    added dependencies for BCrypt

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,26 +1,24 @@
 
-
 public class Book {
-    string bookTitle;
-    string author;
-    double price;
+  String bookTitle;
+  String author;
+  double price;
 
+  public Book(String bookTitle, String author, double price) {
+    this.bookTitle = bookTitle;
+    this.author = author;
+    this.price = price;
+  }
 
-    public Book(string bookTitle, string author, double price){
-      this.bookTitle = bookTitle;
-      this.author = author;
-      this.price = price;
-    }
+  public String getTitle() {
+    return bookTitle;
+  }
 
-    public String getTitle(){
-      return bookTitle;
-    }
+  public String getAuthor() {
+    return author;
+  }
 
-    public String getAuthor(){
-      return author;
-    }
-
-    public double getPrice(){
-      return price;
-    }
+  public double getPrice() {
+    return price;
+  }
 }
