1fb57848af2c8ec234b71441e21936c9078c6225
TylerBajic <tylerbajic18@gmail.com>
2024-11-12 11:40:35 -0600

    Added contents to user class including constructor and methods

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-    
+    private String username;
+    private List<Book> borrowedBooks;
+    private List<Book> reservedBooks;
+    private static final int BORROW_LIMIT = 3; //Avoid excessive global variables (CWE-1108)
+
+    public User(String username) {
+        this.username = username;
+        this.borrowedBooks = new ArrayList<>();
+        this.reservedBooks = new ArrayList<>();
+    }
+
+    public boolean hasReachedBorrowLimit() {
+        return borrowedBooks.size() >= BORROW_LIMIT;
+    }
+
+    public void borrowBook(Book book) {
+        if (!hasReachedBorrowLimit()) {
+            borrowedBooks.add(book);
+        }
+    }
+
+    public void reserveBook(Book book) {
+        reservedBooks.add(book);
+    }
+
+    public boolean hasBorrowed(Book book) {
+        return borrowedBooks.contains(book);
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
 }
